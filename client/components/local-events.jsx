8318c309c88e189778278cd6d5d3317a768c1a7d
Daniel Pham <dnypham@gmail.com>
2021-11-09 23:23:49 +0000

    Create method to render nearby concerts and nearby sporting events. #1

--- client/components/local-events.jsx
+++ client/components/local-events.jsx
@@ -17,7 +17,7 @@ export default class LocalEvents extends React.Component {
       .then(data => {
         this.setState({ ip: data });
         this.setState({ location: data.city + ', ' + data.regionName });
-        fetch('http://api.seatgeek.com/2/events/?geoip=' + data.ip + '&client_id=OTEzNzY5NnwxNjM1Nzk3ODUzLjE2OTAyNTI')
+        fetch('http://api.seatgeek.com/2/events/?per_page=20&geoip=' + data.ip + '&client_id=OTEzNzY5NnwxNjM1Nzk3ODUzLjE2OTAyNTI')
           .then(request => request.json())
           .then(data => {
             this.setState({
@@ -28,12 +28,28 @@ export default class LocalEvents extends React.Component {
       });
   }
 
-  renderEvents() {
-    return this.state.events.map((event, index) => (
+  renderConcerts() {
+    const concerts = this.state.events.filter(event => event.taxonomies[0].name === 'concert');
+
+    return concerts.map((event, index) => (
       <div className="event-card-container" data-id={event.id} key={event.id}>
         <div className="event-image-container">
           <img src={event.performers[0].image}></img>
         </div>
+        <h6 className="local-event-title">{event.title}</h6>
+      </div>
+    ));
+  }
+
+  renderSportingEvents() {
+    const sportingEvents = this.state.events.filter(event => event.taxonomies[0].name === 'sports');
+
+    return sportingEvents.map((event, index) => (
+      <div className="event-card-container" data-id={event.id} key={event.id}>
+        <div className="event-image-container">
+          <img src={event.performers[0].image}></img>
+        </div>
+        <h6 className="local-event-title">{event.title}</h6>
       </div>
     ));
   }
@@ -43,22 +59,40 @@ export default class LocalEvents extends React.Component {
     console.log(this.state.events);
     console.log(this.state.meta);
     return (
-      <div className="local-events-container">
-        <div className="column-full">
-          <div className="local-events-flex">
-            <div className="local-events">
-              <div className="row">
-                <h1 className="local-events-header">{`EVENTS NEAR: ${this.state.location.toUpperCase()}`}</h1>
-              </div>
-              <div className="row">
-                <div className="local-events-card-container">
-                  {this.renderEvents()}
+      <>
+        <div className="local-events-container">
+          <div className="column-full">
+            <div className="local-events-flex">
+              <div className="local-events">
+                <div className="row">
+                  <h1 className="local-events-header">{`CONCERTS NEAR ${this.state.location.toUpperCase()}`}</h1>
+                </div>
+                <div className="row">
+                  <div className="local-events-card-container">
+                    {this.renderConcerts()}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="local-events-container margin-top">
+          <div className="column-full">
+            <div className="local-events-flex">
+              <div className="local-events">
+                <div className="row">
+                  <h1 className="local-events-header">{`SPORTING EVENTS NEAR ${this.state.location.toUpperCase()}`}</h1>
+                </div>
+                <div className="row">
+                  <div className="local-events-card-container">
+                    {this.renderSportingEvents()}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
