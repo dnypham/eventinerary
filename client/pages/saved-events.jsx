bef97b40cb2185524e6a2d30f714ed5c0ec37a64
Daniel Pham <dnypham@gmail.com>
2021-11-14 15:51:39 +0000

    Add layout and containers to SavedEvents view. #4

--- client/pages/saved-events.jsx
+++ client/pages/saved-events.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 export default class SavedEvents extends React.Component {
   render() {
     return (
-      <>
-      </>
+      <div className="saved-events-layout-container">
+        <div className="saved-events-layout-flex">
+          <div className="saved-events-container">
+            <div className="saved-events-list-container">
+              <h2>SAVED EVENTS</h2>
+            </div>
+          </div>
+          <div className="itinerary-container">
+
+          </div>
+        </div>
+      </div>
     );
   }
 }
