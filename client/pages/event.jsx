8b043da16fd7eeb3db546d27f3eb55321462a2ab
Daniel Pham <dnypham@gmail.com>
2021-12-06 18:55:59 +0000

    Clean CSS for 'event.jsx'. #5

--- client/pages/event.jsx
+++ client/pages/event.jsx
@@ -30,38 +30,36 @@ export default class Event extends React.Component {
 
   render() {
     return (
-      <div className="event-info-layout-container">
-        <div className="event-info-layout-flex">
-          <div className="event-info-container">
+      <div className="e-info-lyt-ctr flex-c">
+        <div className="flex-c">
+          <div className="e-info-ctr pos-rel bdr-radius-20px">
             <div className="row">
-              <img className="event-info-image" src={this.props.performer.image}></img>
-              <div className="event-info-header-text-container">
-                <h1 className={'event-info-header-text'}>{this.props.performer.name.toUpperCase()}</h1>
+              <img className="e-info-img bdr-radius-t-l-20px" src={this.props.performer.image}></img>
+              <div className="e-info-hdr-txt-ctr flex-c bdr-radius-t-r-20px">
+                <h1 className="e-info-hdr-txt">{this.props.performer.name.toUpperCase()}</h1>
               </div>
             </div>
-            <div className="event-info-map-container">
-              <div className="flx-center">
+            <div className="e-info-map-ctr flex-c">
+              <div className="flex-c">
                 <div className="map-outline">
                   <iframe src={`https://maps.google.com/maps?q=${this.props.eventInfo.venue.location.lat},${this.props.eventInfo.venue.location.lon}&z=15&output=embed`} width="100%" height="100%"></iframe>
                 </div>
               </div>
             </div>
-            <div className="event-info-text-container">
-              <h2 className="event-card-title">{this.props.eventInfo.title}</h2>
-              <div className="event-card-info-flex-space-between">
-                <div className="event-info-line-space">
-                  <h2 className="event-card-venue-name">{this.props.eventInfo.venue.name}</h2>
-                  <h2 className="event-card-info-address">{`${this.props.eventInfo.venue.address}, ${this.props.eventInfo.venue.extended_address}`}</h2>
-                </div>
-                <div className="event-info-line-space">
-                  <h2 className="event-card-info-date">{convertDateTime(this.props.eventInfo.datetime_local).date}</h2>
-                  <h2 className="event-card-info-time">{convertDateTime(this.props.eventInfo.datetime_local).time}</h2>
+            <div className="e-info-txt-ctr">
+              <h2 className="e-card-title">{this.props.eventInfo.title}</h2>
+              <div className="flex-space-between">
+                <div className="e-info-txt-ctr-2">
+                  <h2 className="ft-18px">{this.props.eventInfo.venue.name}</h2>
+                  <h2 className="ft-16px txt-gray">{`${this.props.eventInfo.venue.address}, ${this.props.eventInfo.venue.extended_address}`}</h2>
+                  <h2 className="ft-18px">{convertDateTime(this.props.eventInfo.datetime_local).date}</h2>
+                  <h2 className="ft-16px txt-gray">{convertDateTime(this.props.eventInfo.datetime_local).time}</h2>
                 </div>
               </div>
             </div>
-            <div className="event-info-btn-container">
-              <button className="tickets-btn">TICKETS</button>
-              <button className="save-event-btn" onClick={this.handleClick}>SAVE EVENT</button>
+            <div className="e-info-btn-ctr bdr-radius-b-l-20px bdr-radius-b-r-20px flex-space-between align-items-c">
+              <button className="tickets-btn e-info-btn ft-atf-franklin-gothic">TICKETS</button>
+              <button className="save-event-btn e-info-btn ft-atf-franklin-gothic" onClick={this.handleClick}>SAVE EVENT</button>
             </div>
           </div>
         </div>
