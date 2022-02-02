c8fa342c3fec88841802d65030d3fd91f1f5368f
Daniel Pham <dnypham@gmail.com>
2022-02-02 21:58:44 +0000

    Change header to display icons depending on the window size. #12

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -13,27 +13,46 @@ export default class Header extends React.Component {
 
   render() {
     return (
-      <header className="header-layout-container flex-c">
-        <div className="col-third">
-          <div className="flex-s">
-            <Searchbar search={this.props.search}/>
+      <>
+        <header className="header-layout-container flex-c">
+          <div className="col-third">
+            <div className="flex-s">
+              <Searchbar search={this.props.search}/>
+            </div>
           </div>
-        </div>
-        <div className="col-third">
-          <div className="flex-c">
-            <h1 className="logo ft-cooper-black" onClick={this.handleClick}>Eventinerary</h1>
+          <div className="col-third">
+            <div className="flex-c">
+              <h1 className="logo ft-cooper-black" onClick={this.handleClick}>Eventinerary</h1>
+            </div>
           </div>
-        </div>
-        <div className="col-third">
-          <div className="flex-e">
-            <nav className="navbar-links">
-              <a className="links ft-atf-franklin-gothic" href="#saved-events">Saved Events</a>
-              <a className="links ft-atf-franklin-gothic" href="#contact">Contact</a>
-              <a className="links ft-atf-franklin-gothic" href="#log-in">Log In</a>
-            </nav>
+          <div className="col-third">
+            <div className="flex-e">
+              <nav className="navbar-links">
+                <a className="links ft-atf-franklin-gothic" href="#saved-events">Saved Events</a>
+                <a className="links ft-atf-franklin-gothic" href="#contact">Contact</a>
+                <a className="links ft-atf-franklin-gothic" href="#log-in">Log In</a>
+              </nav>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+        <header className="header-layout-container-2 flex-c">
+          <div className="col-third">
+            <div className="flex-s">
+              <i className="fas fa-search fa-lg header-icons"></i>
+            </div>
+          </div>
+          <div className="col-third">
+            <div className="flex-c">
+              <h1 className="logo ft-cooper-black" onClick={this.handleClick}>Eventinerary</h1>
+            </div>
+          </div>
+          <div className="col-third">
+            <div className="flex-e">
+              <i className="fas fa-bars fa-lg header-icons"></i>
+            </div>
+          </div>
+        </header>
+      </>
     );
   }
 }
